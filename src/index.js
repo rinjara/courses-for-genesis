cdd591f9490c40022d6ec4dc94d1d1d822e04dc8
Krystyna Sharhorodska <rinjara@gmail.com>
2023-03-15 20:41:44 +0200

    updated basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/courses-for-genesis">
       <App />
     </BrowserRouter>
   </React.StrictMode>
